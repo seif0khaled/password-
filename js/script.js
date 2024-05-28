a5162dbc2a4b4a9fa359a729bd0d0b019fa4bbb3
seif0khaled <seifkhaled1995@gmail.com>
2024-05-29 02:27:07 +0300

    password

---
+++ js/script.js
@@ -0,0 +1,70 @@
+let inputpassword = document.querySelector(".password");
+let strength = document.querySelector("#strength");
+
+let msg = document.querySelector(".message");
+
+inputpassword.addEventListener('input', function () {
+    if (inputpassword.value.length > 0) {
+        msg.style.display = "block";
+    } else {
+        msg.style.display = "none"
+    }
+    if (inputpassword.value.length < 4) {
+        strength.innerHTML = "weak";
+        strength.style.color = "red";
+    } else if (inputpassword.value.length >= 4 && inputpassword.value.length < 8) {
+        strength.innerHTML = "medium";
+        strength.style.color = "yellow";
+    }
+    else if (inputpassword.value.length >= 8) {
+        strength.innerHTML = "strong";
+        strength.style.color = "green";
+    }
+
+
+    let eyeicon = document.getElementById("eyeicon");
+        let password = document.getElementById("password");
+
+        eyeicon.onclick = function(){
+
+            if(password.type=="password"){
+                password.type = "text";
+                eyeicon.src = "eye-open.png"
+            }else{
+                password.type = "password"
+                eyeicon.src = "eye-close.png"
+            }
+
+        }
+
+  
+
+
+}
+    //console.log(inputpassword.);
+)
+let cart = ["milk","chips","coffe","chocolate"]
+
+console.log(cart)
+
+for(let i=0; i< cart.length; i++){
+    console.log(cart[i]);
+}
+
+// console.log(inputpassword);
+
+function example(firstName, lastName){
+    let sayhi = "hello" + firstName + lastName;
+    console.log(sayhi);
+}
+
+example("pierre", "Emad");
+
+example("mohamed","Ahmed")
+
+
+function sayhello(){
+    console.log("hello");
+}
+
+document.querySelector("button").addEventListener("click",sayhello);
